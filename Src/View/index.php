618dddf9ea807209828ecdd79c4f7323680bde3d
Luis Carlos <luiscarlos@bithelp.tech>
2021-12-17 19:05:46 -0300

    refatorando laiunt

--- Src/View/index.php
+++ Src/View/index.php
@@ -6,37 +6,39 @@ require_once'../../vendor/autoload.php';
 use Src\Modulos\EntrarAnoSairSeculo\AnoParaSeculo;
 use Src\Modulos\Primos\PrimosSemVrsEntradas;
 
-$digiteAnoParaconverterEmSeculo=20265;
-$primosV1=5;
-$primosV2=20;
+$digiteAnoParaconverterEmSeculo = 20265;
+$primosV1 = 5;
+$primosV2 = 20;
 
-$anoParaSeculo= new AnoParaSeculo($digiteAnoParaconverterEmSeculo);
-$seculo = $anoParaSeculo->seculo;
-echo "O Século do ano $digiteAnoParaconverterEmSeculo = $seculo<br/>";
-echo"Primos com valores informados retirado:";
+$anoParaSeculo = new AnoParaSeculo ($digiteAnoParaconverterEmSeculo);
+$seculo = $anoParaSeculo -> seculo;
+echo "O Século do ano $digiteAnoParaconverterEmSeculo = $seculo <br/>" ;
+echo "Primos com valores informados retirado: " ;
  
-$primosExcetoValorDeEntrada=new PrimosSemVrsEntradas($primosV1,$primosV2);
-foreach($primosExcetoValorDeEntrada->primosSemEntradas as $valor){
-    echo $valor.",";
+$primosExcetoValorDeEntrada = new PrimosSemVrsEntradas ($primosV1,$primosV2);
+foreach ($primosExcetoValorDeEntrada -> primosSemEntradas as $valor){
+    echo $valor . "," ;
 }
 
-echo "<br/> O Array de valores aleatórios são : ";
+echo "<br/> O Array de valores aleatórios são :" ;
 
-$numerosAleatorios=array();
-$indiceNumerosAleatorios;
-$numerosNaoRepetidos=array();
+$numerosAleatorios = array() ;
+$indiceNumerosAleatorios ;
+$numerosNaoRepetidos = array() ;
 
-for($i=0;$i<20;$i++){    
-array_push($numerosAleatorios,rand(1,10));
-echo $numerosAleatorios[$i].",";
+for ($i = 0 ; $i < 20 ; $i++){    
+array_push ($numerosAleatorios, rand(1,10));
+echo $numerosAleatorios [$i] . ",";
 }
 
-$contagemNumeros=array_count_values($numerosAleatorios);
-$indiceNumerosAleatorios=array_keys($contagemNumeros);
-echo"<br/>Dos 20 números aleatórios os que não se repetem são: "; 
-foreach($indiceNumerosAleatorios as $key){
-    if($contagemNumeros[$key]==1){
-        array_push($numerosNaoRepetidos,$key);
-        echo"$key,";
+$contagemNumeros = array_count_values ($numerosAleatorios) ;
+$indiceNumerosAleatorios = array_keys ($contagemNumeros) ;
+echo "<br/>Dos 20 números aleatórios os que não se repetem são:" ; 
+foreach ($indiceNumerosAleatorios as $key){
+    if($contagemNumeros [$key] == 1){
+        array_push ($numerosNaoRepetidos, $key);
+        echo "$key," ;
     }
 } 
+
+
