b5a7b76e04acabce484584d6546edcf83652c462
Luis Carlos <luiscarlos@bithelp.tech>
2021-12-17 15:59:25 -0300

    atualizando repositorio

--- Src/View/index.php
+++ Src/View/index.php
@@ -6,11 +6,25 @@ require_once'../../vendor/autoload.php';
 use Src\Modulos\EntrarAnoSairSeculo\AnoParaSeculo;
 use Src\Modulos\Primos\PrimosSemVrsEntradas;
 
-$anoParaSeculo= new AnoParaSeculo("101");
+$digiteAnoParaconverterEmSeculo=20265;
+$primosV1=5;
+$primosV2=20;
+
+$anoParaSeculo= new AnoParaSeculo($digiteAnoParaconverterEmSeculo);
 $seculo = $anoParaSeculo->seculo;
-echo "$seculo<br/>";
+echo "O Século do ano $digiteAnoParaconverterEmSeculo = $seculo<br/>";
+echo"Primos com valores informados retirado:";
  
-$primosExcetoValorDeEntrada=new PrimosSemVrsEntradas(4,97);
+$primosExcetoValorDeEntrada=new PrimosSemVrsEntradas($primosV1,$primosV2);
 foreach($primosExcetoValorDeEntrada->primosSemEntradas as $valor){
     echo $valor.",";
+}
+echo "<br/> O Array de valores aleatórios são : ";
+
+$numeros=array();
+for($cont=0;$cont<20;$cont++){
+    for($i=0;$i<20;$i++){    
+        array_push($numeros,rand(1,10));        
+    }
+    echo $numeros[$cont].",";
 }
